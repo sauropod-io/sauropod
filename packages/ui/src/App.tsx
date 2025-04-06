5fed6676b233fe2bf103c0f8003a8a03afb20643
Stephan Wolski <steph@sauropod.io>
2025-04-06 14:49:39 -0700

    feat(ui): lazy pages

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,3 +1,4 @@
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router";
 
 import AppSidebar from "@/components/AppSidebar";
@@ -6,34 +7,96 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import Logs from "@/pages/Logs";
-import Settings from "@/pages/Settings";
-import Task from "@/pages/Task";
-import TaskList from "@/pages/TaskList";
-import Workflow from "@/pages/Workflow";
-import WorkflowList from "@/pages/WorkflowList";
 import { INDEX, LOGS, SETTINGS, taskRoute, workflowRoute } from "@/routes";
+
+const Logs = lazy(() => import("@/pages/Logs"));
+const Settings = lazy(() => import("@/pages/Settings"));
+const Task = lazy(() => import("@/pages/Task"));
+const TaskList = lazy(() => import("@/pages/TaskList"));
+const Workflow = lazy(() => import("@/pages/Workflow"));
+const WorkflowList = lazy(() => import("@/pages/WorkflowList"));
+
+/** Suspense wrapper. */
+function SupportLoading({ children }: { children: React.ReactNode }) {
+  return <Suspense fallback={<div>Loading page...</div>}>{children}</Suspense>;
+}
 
 function PageContent() {
   return (
     <Routes>
       <Route path="*" element={<div>Not found</div>} />
       <Route path={INDEX} element={<Navigate to={workflowRoute("")} />} />
-      <Route path={taskRoute("new")} element={<Task />} />
-      <Route path={taskRoute(":id")} element={<Task />} />
-      <Route path={taskRoute("")} element={<TaskList />} />
-      <Route path={workflowRoute("new")} element={<Workflow />} />
-      <Route path={workflowRoute(":id")} element={<Workflow />} />
-      <Route path={workflowRoute("")} element={<WorkflowList />} />
-      <Route path={LOGS} element={<Logs />} />
-      <Route path={SETTINGS} element={<Settings />} />
+      <Route
+        path={taskRoute("new")}
+        element={
+          <SupportLoading>
+            <Task />
+          </SupportLoading>
+        }
+      />
+      <Route
+        path={taskRoute(":id")}
+        element={
+          <SupportLoading>
+            <Task />
+          </SupportLoading>
+        }
+      />
+      <Route
+        path={taskRoute("")}
+        element={
+          <SupportLoading>
+            <TaskList />
+          </SupportLoading>
+        }
+      />
+      <Route
+        path={workflowRoute("new")}
+        element={
+          <SupportLoading>
+            <Workflow />
+          </SupportLoading>
+        }
+      />
+      <Route
+        path={workflowRoute(":id")}
+        element={
+          <SupportLoading>
+            <Workflow />
+          </SupportLoading>
+        }
+      />
+      <Route
+        path={workflowRoute("")}
+        element={
+          <SupportLoading>
+            <WorkflowList />
+          </SupportLoading>
+        }
+      />
+      <Route
+        path={LOGS}
+        element={
+          <SupportLoading>
+            <Logs />
+          </SupportLoading>
+        }
+      />
+      <Route
+        path={SETTINGS}
+        element={
+          <SupportLoading>
+            <Settings />
+          </SupportLoading>
+        }
+      />
     </Routes>
   );
 }
 
 function App() {
   return (
-    <div className="flex  h-screen w-full bg-background">
+    <div className="flex h-screen w-full bg-background">
       <SidebarProvider defaultOpen={true}>
         <AppSidebar />
         <SidebarInset className="flex-1 overflow-auto">
